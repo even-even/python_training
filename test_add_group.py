65ab82a4b9c0c5b6e07e4add405fe8e8efcb5476
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-07-12 19:19:38 +0300

    rework step to method

--- test_add_group.py
+++ test_add_group.py
@@ -12,7 +12,18 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php")
+
+    def login(self, wd):
+        # login in site (admin/secret)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -20,8 +31,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
+        # input group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("123")
@@ -31,8 +48,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("asdfgh")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+        # return to group page
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        # logout
         wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
