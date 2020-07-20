333d9177784107342e5c1f6483a07f8d0ea02961
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-07-20 23:30:55 +0300

    unification def in test_add_group.py

--- test_add_group.py
+++ test_add_group.py
@@ -11,10 +11,9 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
-
     def login(self, wd, username, password):
+        # open home page
+        wd.get("http://localhost/addressbook/group.php")
         # login in site (admin/secret)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -24,10 +23,9 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
     def create_group(self, wd, group):
+        # open group page
+        wd.find_element_by_link_text("groups").click()
         # input group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -42,8 +40,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-
-    def return_to_group_page(self, wd):
         # return to group page
         wd.find_element_by_link_text("group page").click()
 
@@ -53,20 +49,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name = "123", header = "qwerty", footer = "asdfgh"))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name = "", header = "", footer = ""))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):
