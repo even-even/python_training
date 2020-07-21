0573cc2beb52256a4b0726ec79b369f276abc2da
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-07-21 23:20:05 +0300

    add test_del_group.py and add fixture/group.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.add_user import User
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_user(app):
