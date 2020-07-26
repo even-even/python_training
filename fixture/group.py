90fb20a24c2e1b001ae5a3169dad020a85b550e4
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-07-26 20:32:08 +0300

    improve edit groupInfo and userInfo

--- fixture/group.py
+++ fixture/group.py
@@ -9,6 +9,15 @@ class GroupHelper:
         # input group creation
         wd.find_element_by_name("new").click()
         # fill group form
+        self.edit_groupInfo(group, wd)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        # return to group page
+        wd.find_element_by_link_text("group page").click()
+
+    def edit_groupInfo(self, group, wd):
+        """ edit Info about group.
+        Use in create and edit groupInfo"""
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -18,10 +27,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        # return to group page
-        wd.find_element_by_link_text("group page").click()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -41,15 +46,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
 
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.edit_groupInfo(group, wd)
         wd.find_element_by_name("update").click()
 
         # return to group page
--- fixture/user.py
+++ fixture/user.py
@@ -5,11 +5,10 @@ class UserHelper:
     def __init__(self, app):
         self.app = app
 
-    def create_user(self, user):
-        wd = self.app.wd
-        # click Add new
-        wd.find_element_by_link_text("add new").click()
-        # create user
+    def edit_userInfo(self, user, wd):
+        """ edit Info about user.
+        Use in create and edit userInfo"""
+
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(user.firstname)
@@ -64,6 +63,12 @@ class UserHelper:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(user.notes)
+
+    def create_user(self, user):
+        wd = self.app.wd
+        # click Add new
+        wd.find_element_by_link_text("add new").click()
+        self.edit_userInfo(user, wd)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         # return Home page
         wd.find_element_by_link_text("home page").click()
@@ -76,61 +81,7 @@ class UserHelper:
 
         # edit user
         wd.find_element(By.XPATH, "//img[@alt=\'Edit\']").click()
-
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(user.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(user.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(user.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(user.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(user.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(user.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(user.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(user.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(user.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(user.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(user.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(user.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(user.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(user.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(user.homepage)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(user.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(user.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(user.notes)
+        self.edit_userInfo(user, wd)
         wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
         # return Home page
         wd.find_element_by_link_text("home page").click()
