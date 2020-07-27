a644f372254f75f7daca6861897224a0d0cf71a7
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-07-27 17:07:09 +0300

    improve fixture logout

--- fixture/application.py
+++ fixture/application.py
@@ -12,5 +12,12 @@ class Application:
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
 
+    def is_valid(self):
+        try:
+            self.wd.current_url()
+            return True
+        except:
+            return False
+
     def destroy(self):
         self.wd.quit()
