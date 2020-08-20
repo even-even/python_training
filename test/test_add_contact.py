d85035852807b5884c485c316191342b4b828490
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-08-21 02:26:32 +0300

    add json testdata

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,67 +1,12 @@
+# -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
-import datetime
-import re
 
 
-def random_string(maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 5
-    s = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    # to avoid test fail because of symbol '
-    return re.sub("'", "", s)
-
-
-def random_text_string(maxlen):
-    symbols = string.ascii_letters
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_number_string(minlen, maxlen):
-    symbols = string.digits
-    return "".join([random.choice(symbols) for i in range(random.randrange(minlen, maxlen))])
-
-
-def random_date(start_date, end_date):
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days = random_number_of_days)
-    return random_date
-
-
-def random_email(length):
-    domains = ["hotmail.com", "gmail.com", "aol.com", "mail.com", "mail.kz", "yahoo.com"]
-    letters = string.ascii_lowercase[:12] + string.digits
-
-    def get_random_domain(domains):
-        return random.choice(domains)
-
-    def get_random_name(letters, length):
-        return "".join(random.choice(letters) for i in range(length))
-
-    def generate_random_email(length):
-        return get_random_name(letters, length) + "@" + get_random_domain(domains)
-
-    return generate_random_email(length)
-
-
-testdata = [
-    Contact(lastname = random_text_string(20), firstname = random_text_string(20), middlename = random_text_string(20),
-            nickname = random_string(10), company = random_text_string(20), address = random_string(50),
-            email = random_email(10), home = random_number_string(0, 10),
-            mobile = random_number_string(5, 12), work = random_number_string(0, 4), phone2 = random_number_string(0, 2)
-            )
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids = [repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key = Contact.id_or_max) == sorted(new_contacts, key = Contact.id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
