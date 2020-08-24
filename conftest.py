6b8a7f5e2e7417fe6c0b686884bfa9e810d33284
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-08-24 11:39:57 +0300

    check_ui

--- conftest.py
+++ conftest.py
@@ -25,15 +25,16 @@ def app(request):
     browser = request.config.getoption("--browser")
     web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
-    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
+        fixture = Application(browser = browser, base_url = web_config['baseUrl'])
+    fixture.session.ensure_login(username = web_config['username'], password = web_config['password'])
     return fixture
 
 
 @pytest.fixture(scope = "session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    dbfixture = DbFixture(host = db_config['host'], name = db_config['name'], user = db_config['user'],
+                          password = db_config['password'])
 
     def fin():
         dbfixture.destroy()
@@ -52,9 +53,15 @@ def stop(request):
     return fixture
 
 
+@pytest.fixture()
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action = "store", default = "firefox")
     parser.addoption("--target", action = "store", default = "target.json")
+    parser.addoption("--check_ui", action = "store_true")
 
 
 def pytest_generate_tests(metafunc):
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 from random import randrange
 
 
-def test_delete_some_group(app):
+def test_delete_some_group(app, db, check_ui):
     if app.group.count() == 0:
         app.group.create(Group(name = "group_for_delete"))
     old_groups = app.group.get_group_list()
@@ -12,3 +12,5 @@ def test_delete_some_group(app):
     new_groups = app.group.get_group_list()
     old_groups[index:index+1] = []
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key = Group.id_or_max) == sorted(app.group.get_group_list(), key = Group.id_or_max)
