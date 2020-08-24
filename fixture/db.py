4cd9571232d72a40f076df8999749205687e3139
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-08-25 01:25:49 +0300

    rework test info homepage

--- fixture/db.py
+++ fixture/db.py
@@ -10,7 +10,8 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+        self.connection = pymysql.connect(host = host, database = name, user = user, password = password,
+                                          autocommit = True)
 
     def get_group_list(self):
         db_list = []
@@ -19,7 +20,7 @@ class DbFixture:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                db_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+                db_list.append(Group(id = str(id), name = name, header = header, footer = footer))
         finally:
             cursor.close()
         return db_list
@@ -28,10 +29,13 @@ class DbFixture:
         db_list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute(
+                "select id, firstname, lastname, address, home, mobile, work, email, email2, email3  from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                db_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3) = row
+            db_list.append(Contact(id = str(id), firstname = firstname, lastname = lastname, address = address,
+                                   home = home, mobile = mobile, work = work, email = email, email2 = email2,
+                                   email3 = email3))
         finally:
             cursor.close()
         return db_list
